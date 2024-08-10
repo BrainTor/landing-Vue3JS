a53d973ac5ed9c89b7579d6fdc2f95dcd200f00d
BrainTor <proguke@gmail.com>
2024-08-10 21:33:11 +0300

    first commit

---
+++ src/components/Footer_Component.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="footer_container">
+        <center>
+            <h1 style="padding-top: 20px;padding-bottom: 20px;">Я всегда готов к общению! Свяжитесь со мной</h1>
+        </center>
+
+        <div class="main_row">
+            
+                <p class="email_phone">+7-960-459-34-45 </p>
+                <p class="email_phone">+7-906-565-30-09</p>
+                <p class="email_phone">m.yacenko@gmail.com</p>
+            
+                <div class="social_row">
+                    <button class="social_button">
+                        <img class="social_img" src="@/assets/img/icons/vk.png">
+                    </button>
+                    <button class="social_button">
+                        <img class="social_img" src="@/assets/img/icons/telegram.png">
+                    </button>
+                    <button class="social_button" title="Запрещенная на территории РФ соц. сеть  :)">
+                        <img class="social_img" src="@/assets/img/icons/instagram.png">
+                    </button>
+                </div>
+      
+          
+        </div>
+
+        <div class="have_a_question">
+            <p style="font-size: 20px; font-weight: 580;">Остальсь вопросы?</p>
+            <button class="connect_button">Свяжитесь со мной</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Footer_Component'
+}
+</script>
+
+<style scoped>
+.footer_container{
+    width: 100%;
+    background-color:black;
+    color: white;
+    font-family: "PT Serif", serif;
+    padding-top: 60px;
+ 
+}
+.have_a_question{
+    color: black;
+    background-color: var(--main-color);
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 20px;
+    align-items: center;
+}
+.main_row{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 20px;
+}
+.social_button{
+    border: 0px;
+    background-color: transparent;
+    margin-top: 20px;
+    cursor: pointer;
+  
+}
+.social_img{
+    width: 42px;
+}
+.main_col{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+
+}
+.social_row{
+    padding-bottom: 60px;
+}
+
+@keyframes shake {
+    0%{
+        transform: rotate(0deg) scale(1.1);
+    }
+    50%{
+        transform: rotate(4deg);
+    }
+    100%{
+        transform: rotate(-4deg) scale(1);
+    }
+}
+
+@media (hover: hover) {
+    .social_button:hover{
+        animation-name: shake;
+        animation-duration: 1s;
+        animation-iteration-count: 1;
+        animation-direction: alternate;
+        animation-timing-function: ease-in-out;
+    }
+    .connect_button:hover{
+        background-color: var(--main-color);
+    
+    }
+
+  }
+  .email_phone{
+    font-size: 24px;
+    margin-top: 1.2rem;
+  }
+  .connect_button{
+    background-color: transparent;
+    font-size: 20px;
+    border: 0px;
+    cursor: pointer;
+    transition: 0.6s all;
+    padding: 7px 12px;
+    border-radius: 8px;
+  }
+
+</style>
---
+++ src/components/Nav_Component.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="nav_bar">
+        <div class="logo" >
+            <img src="../assets/img/logo.png" style="width: 40px;" alt="">
+            <p class="logo_text">Silver-tech</p>
+        </div>
+
+        <div class="info">
+            <button class="button_info" @click="handle_change('_about')"><fa icon="circle-info" /> Обо мне</button>
+            <button class="button_info" @click="handle_change('_study')"><fa icon="book" /> Обучение</button>
+            <button class="button_info" @click="handle_change('_code')"><fa icon="code" /> Разработка</button>
+            <button class="button_info button_contact" @click="handle_change('_contact')"><fa icon="address-book" /> Контакты</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Nav_Component',
+    methods:{
+        handle_change(name){
+            this.$emit('scroll_to', name);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .nav_bar{
+        height: 70px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: var(--background_nav_bar);
+        color: var(--text-color_nav);
+        width: 100%;
+        opacity: 1;
+
+    }
+    .logo_text{
+        font-family: "Edu AU VIC WA NT Hand", cursive;
+        font-size: 18px;
+        font-weight: 700;
+        margin-left: 7px;
+    }
+    .logo{
+        display: flex;
+        align-items: center;
+        margin-left: 20px;
+    }
+    .button_info{
+        background-color: transparent;
+        color: var(--text-color_nav);
+        font-family: "PT Serif", serif;
+        font-size: 16.5px;
+        border: 0px;
+        cursor: pointer;
+        transition:0.3s all;
+        margin-left: 0.7rem;
+        margin-right: 0.7rem;
+        padding: 7px 12px;
+        border-radius: 7px;
+    }
+
+    .button_contact{ 
+        background-color: #d43a63 !important;
+        color: rgb(255, 254, 254);
+        margin-right: 20px;
+    }
+
+    @media (hover: hover) {
+        .button_info:hover {
+          background-color: var(--hover-color); 
+          color: black;
+        }
+        .button_contact:hover{
+            background-color: #c01a46 !important;
+            color: white;
+        }
+      }
+      
+      @media (hover: none) {
+        .button_info:active {
+        background-color: var(--hover-color); ; 
+        }
+        .button_contact:active{
+            background-color: #c01a46 !important;
+            color: white;
+        }
+      }
+</style>
